bbe0d11ddc456213a8a790c74b07435f9d893e3e
Pedro Balaguer <pedro.balaguer@hotmail.com>
2020-06-23 00:58:49 -0300

    Add frog jump mechanic
    Basic version of the jump mechanic: A and D keys to charge left and right leg respectively and then the jump angle and force are calculated from both keys charging times.

---
+++ Assets/Scripts/FrogControls.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogControls : MonoBehaviour
+{
+    // Contants that should be initialized from th GUI
+    public float MAX_JUMP_CHARGE = 300.0f;
+    public float JUMP_FORCE = 1000.0f;
+
+    private float leftForce = 0.0f;
+    private float rightForce = 0.0f;
+
+    private Rigidbody2D m_RigidBody;
+
+    void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Jump()
+    {
+        if (leftForce == 0 && rightForce == 0) return;
+
+        float angle = (leftForce - rightForce) / (leftForce + rightForce) * 90;
+        float force = Mathf.Max(leftForce, rightForce) / MAX_JUMP_CHARGE * JUMP_FORCE;
+        float xForce = Mathf.Sin(angle * Mathf.PI / 180.0f) * force;
+        float yForce = Mathf.Cos(angle * Mathf.PI / 180.0f) * force;
+        Vector2 jumpVector = new Vector2(xForce, yForce);
+        m_RigidBody.AddForce(jumpVector);
+
+        // Reset both legs
+        leftForce = 0;
+        rightForce = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("LeftLeg"))
+        {
+            leftForce += 1.0f;
+        }
+
+        if (Input.GetButton("RightLeg"))
+        {
+            rightForce += 1.0f;
+        }
+
+        if (Input.GetButtonUp("LeftLeg") || Input.GetButtonUp("RightLeg")
+            || rightForce >= MAX_JUMP_CHARGE || leftForce >= MAX_JUMP_CHARGE)
+        {
+            Jump();
+        }
+
+    }
+}

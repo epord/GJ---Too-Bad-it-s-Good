b5726dfb2dc79f06d29bad7bd10172afe0326717
Diego de Rochebouet <ddegrimaudetderocheb@itba.edu.ar>
2020-07-02 01:45:56 -0300

    Add menu and soundMixer

---
+++ Assets/Scripts/OptionMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+    
+    public void SetMusicVolume(float volume)
+    {
+        
+        setVolume("MusicVolume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        setVolume("SFXVolume", volume);
+    }
+
+    private void setVolume(string name, float volume)
+    {
+        float newVolume = Mathf.Log10(volume) * 20;
+        audioMixer.SetFloat(name, newVolume);
+    }
+}

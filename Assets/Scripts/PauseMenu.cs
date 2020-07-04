68806e0cf2d5392b92e7468c6a10bc7f299e807c
Diego de Rochebouet <ddegrimaudetderocheb@itba.edu.ar>
2020-07-04 17:07:23 -0300

    Add Pause Menu to Game
    In the pause menu we can access to options, resume game or return to
    main menu.
    Would be cool to add a fade animation when going in to the menu.
    Maybe also change the music to something less loud?
    Add button changes to options menu
    Add that the music slider has the current value
    Add an accesibility menu to make it easier

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject optionMenuUI;
+    public GameObject backgroundUI;
+
+    void Start()
+    {
+        Time.timeScale = 1f;
+        GamePaused = false ;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("MenuButton"))
+        {
+            if (GamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        backgroundUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+    
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        optionMenuUI.SetActive(false);
+        backgroundUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false ;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}

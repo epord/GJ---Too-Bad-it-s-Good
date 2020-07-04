438bda86ef76d6fb6fd6d6222bc304ebf3934ffc
Diego de Rochebouet <ddegrimaudetderocheb@itba.edu.ar>
2020-07-04 20:34:27 -0300

    Keep user preferences for volume

--- Assets/Scripts/OptionMenu.cs
+++ Assets/Scripts/OptionMenu.cs
@@ -2,26 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionMenu : MonoBehaviour
 {
 
-    public AudioMixer audioMixer;
+    public const float DEFAULT_VOLUME = 0.75f;
     
+    public AudioMixer AudioMixer;
+    public Slider MusicSlider;
+    public Slider SFXSlider;
+
+    private void Start()
+    {
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume", DEFAULT_VOLUME);
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume", DEFAULT_VOLUME);
+    }
+
     public void SetMusicVolume(float volume)
     {
-        
+        PlayerPrefs.SetFloat("MusicVolume", volume);
         setVolume("MusicVolume", volume);
     }
 
     public void SetSFXVolume(float volume)
     {
+        PlayerPrefs.SetFloat("SFXVolume", volume);
         setVolume("SFXVolume", volume);
     }
 
     private void setVolume(string name, float volume)
     {
         float newVolume = Mathf.Log10(volume) * 20;
-        audioMixer.SetFloat(name, newVolume);
+        AudioMixer.SetFloat(name, newVolume);
     }
 }
